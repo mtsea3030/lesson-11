42f355caabd8b7b64f267861006d0a932e167aaf
Mark.Sealey <yourmanmark@outlook.com>
2024-08-28 12:35:24 -0500

    commit remove

--- main.tf
+++ main.tf
@@ -33,15 +33,15 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
 
-resource "aws_iam_user" "testuser_99" {
-  name = "testuser-99"
-}
+# resource "aws_iam_user" "testuser_99" {
+#   name = "testuser-99"
+# }
